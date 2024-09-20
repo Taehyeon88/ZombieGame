f51fa5704670dc5f0ad29f916a3f03e9071e8760
Taehyeon88 <Taehyeon5376@naver.com>
2024-09-20 11:48:34 +0900

    [Feat]2주차 코딩추가

--- Zombie (1)/Assets/Scripts/Gun.cs
+++ Zombie (1)/Assets/Scripts/Gun.cs
@@ -82,7 +82,7 @@ public class Gun : MonoBehaviour {
             //충돌한 상대방으로부터 IDamageable 오브젝트 가져오기 시도
             IDamageable target = hit.collider.GetComponent<IDamageable>();
 
-            if(target != null )
+            if(target != null)
             {
                 target.OnDamage(gunData.damage, hit.point, hit.normal);
             }
@@ -90,7 +90,7 @@ public class Gun : MonoBehaviour {
         }
         else
         {
-            //레이가 다른 물체롸 충공하지 않았다면
+            //레이가 다른 물체와 충공하지 않았다면
             //탄알이 최대 사정거리까지 날아갔을 때의 위치로 사용
             hitPosition = fireTransform.position + fireTransform.forward * fireDistance;
         }
@@ -113,7 +113,7 @@ public class Gun : MonoBehaviour {
         shellEjectEffect.Play();
 
         //총격 소리 재생
-        gunAudioPlayer.PlayOneShot(gunData.shotClip);
+        gunAudioPlayer.PlayOneShot(gunData.shotClip); //사운드 중첩이 가능하다(Play()는 재생하던 사운드 정지후, 재생)
 
         //선의 시작점은 총구의 위치
         bulletLineRenderer.SetPosition(0, fireTransform.position);
@@ -131,16 +131,41 @@ public class Gun : MonoBehaviour {
 
     // 재장전 시도
     public bool Reload() {
-        return false;
+        if (state == State.Ready || ammoRemain <= 0 || magAmmo >= gunData.magCapacity)
+        {
+            //이미 재장전 중이거나 남은 탄알이 없거나
+            //탄창에 탄알이 이미 가득한 경우 재장전할 수 없음
+            return false;
+        }
+        StartCoroutine(ReloadRoutine());
+        return true;
     }
 
     // 실제 재장전 처리를 진행
     private IEnumerator ReloadRoutine() {
         // 현재 상태를 재장전 중 상태로 전환
         state = State.Reloading;
-      
+
+        //재장전 소리 재생
+        gunAudioPlayer.PlayOneShot(gunData.reloadClip);
+
         // 재장전 소요 시간 만큼 처리 쉬기
         yield return new WaitForSeconds(gunData.reloadTime);
+
+        //탄창에 채울 탄알 계산
+        int ammoToFill = gunData.magCapacity - magAmmo;
+
+        //탄창에 채워야할 탄알이 남은 탄알보다 많다면
+        //채워야 할 탄알 수를 남은 탄알 수에 맞춰 줄임
+        if (ammoRemain <= ammoToFill)
+        {
+            ammoToFill = ammoRemain;
+        }
+
+        //탄알을 채움
+        magAmmo += ammoToFill;
+        //남은 탄알에서 탄창에 채운만큼 탄알을 뺌
+        ammoRemain -= ammoToFill;
 
         // 총의 현재 상태를 발사 준비된 상태로 변경
         state = State.Ready;
